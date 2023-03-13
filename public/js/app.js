1fcfb0841074734dcdb50d1dc380a6a2a178978a
soojlee0106 <soojlee0106@naver.com>
2023-03-14 00:18:36 +0900

    Set up basic logic with weights

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,24 @@
 function submitForm() {
-    const choice1 = document.getElementById('choice1').value;
-    const choice2 = document.getElementById('choice2').value;
+    const choice1_pro = document.getElementById('choice1_pro').value;
+    const choice1_con = document.getElementById('choice1_con').value;
+    const choice1_pro_weight = document.getElementById('choice1_pro_weight').value;
+    const choice1_con_weight = document.getElementById('choice1_con_weight').value;
+
+    const choice2_pro = document.getElementById('choice2_pro').value;
+    const choice2_con = document.getElementById('choice2_con').value;
+    const choice2_pro_weight = document.getElementById('choice2_pro_weight').value;
+    const choice2_con_weight = document.getElementById('choice2_con_weight').value;
+
     const resultDiv = document.getElementById('result');
 
-    if (choice1 > choice2) {
-        resultDiv.textContent = "choice1";
-    } else if (choice2 > choice1) {
-        resultDiv.textContent = "choice2";
+    const choice1_sum = parseInt(choice1_pro) * parseInt(choice1_pro_weight) + parseInt(choice1_con) * parseInt(choice1_con_weight) - 1;
+    const choice2_sum = parseInt(choice2_pro) * parseInt(choice2_pro_weight) + parseInt(choice2_con) * parseInt(choice2_con_weight) - 1;
+
+    if (choice1_sum > choice2_sum) {
+        resultDiv.innerHTML = 'Choice 1 is better';
+    } else if (choice1_sum < choice2_sum) {
+        resultDiv.innerHTML = 'Choice 2 is better';
     } else {
-        resultDiv.textContent = "They are equal";
+        resultDiv.innerHTML = 'Both are equal';
     }
 }
