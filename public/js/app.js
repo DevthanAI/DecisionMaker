30a00471ae73d510ed2d314331174732d61f6bd2
Soo <soojlee0106@naver.com>
2023-03-15 10:08:25 +0900

    Make text editable and save as js variable

--- public/js/app.js
+++ public/js/app.js
@@ -12,6 +12,10 @@ function submitForm() {
     const choice1_sum = choice1.pro * choice1.pro_weight + choice1.con * choice1.con_weight - 1;
     const choice2_sum = choice2.pro * choice2.pro_weight + choice2.con * choice2.con_weight - 1;
 
+    var subtitle1 = document.getElementById("pink-subtitle-1").textContent;
+    var subtitle2 = document.getElementById("pink-subtitle-2").textContent;
+    var subtitle3 = document.getElementById("pink-subtitle-3").textContent;
+
     const resultDiv = document.getElementById('result');
     if (choice1_sum > choice2_sum) {
         resultDiv.textContent = 'Choice 1 is better';
